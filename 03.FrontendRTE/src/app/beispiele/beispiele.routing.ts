39a3bb3f87961b583b608d8fd39df56320fd8107
miguelfranken <miguel.franken@rwth-aachen.de>
2020-02-18 20:57:31 +0100

    added empty layout example pages

--- 03.FrontendRTE/src/app/beispiele/beispiele.routing.ts
+++ 03.FrontendRTE/src/app/beispiele/beispiele.routing.ts
@@ -17,6 +17,11 @@ import { BeispieleButtonsGenComponent } from "./beispiele-buttons-gen.component"
 import { BeispieleTextinputsGenComponent } from "./beispiele-textinputs-gen.component";
 import { BeispieleAutocompletesAndDropdownsGenComponent } from "./beispiele-autocompletes-and-dropdowns-gen.component";
 import { BeispieleLabelsGenComponent } from "./beispiele-labels-gen.component";
+import { BeispieleLayout1GenComponent } from "@targetgui/beispiele-layout-1-gen.component/beispiele-layout-1-gen.component";
+import { BeispieleLayout2GenComponent } from "@targetgui/beispiele-layout-2-gen.component/beispiele-layout-2-gen.component";
+import { BeispieleLayout3GenComponent } from "@targetgui/beispiele-layout-3-gen.component/beispiele-layout-3-gen.component";
+import { BeispieleLayout4GenComponent } from "@targetgui/beispiele-layout-4-gen.component/beispiele-layout-4-gen.component";
+import { BeispieleLayout5GenComponent } from "@targetgui/beispiele-layout-5-gen.component/beispiele-layout-5-gen.component";
 
 export const routes: Routes = [
   {
@@ -87,6 +92,26 @@ export const routes: Routes = [
   {
     path: 'layout',
     component: BeispieleLayoutGenComponent,
+  },
+  {
+    path: 'layout1',
+    component: BeispieleLayout1GenComponent,
+  },
+  {
+    path: 'layout2',
+    component: BeispieleLayout2GenComponent,
+  },
+  {
+    path: 'layout3',
+    component: BeispieleLayout3GenComponent,
+  },
+  {
+    path: 'layout4',
+    component: BeispieleLayout4GenComponent,
+  },
+  {
+    path: 'layout5',
+    component: BeispieleLayout5GenComponent,
   },
   {
     path: 'navigation',
