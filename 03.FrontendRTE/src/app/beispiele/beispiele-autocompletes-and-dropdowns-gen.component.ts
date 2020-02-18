b8ce20e837bfbc2addfd01b0fd52067d2120a595
miguelfranken <miguel.franken@rwth-aachen.de>
2020-02-18 18:44:37 +0100

    added dropdown & autocomplete control examples with placeholder

--- 03.FrontendRTE/src/app/beispiele/beispiele-autocompletes-and-dropdowns-gen.component.ts
+++ 03.FrontendRTE/src/app/beispiele/beispiele-autocompletes-and-dropdowns-gen.component.ts
@@ -39,7 +39,9 @@ export class BeispieleAutocompletesAndDropdownsGenComponent extends BeispieleAut
     ];
 
     this._myDropdownInput1Control.setOptions(this.years);
+    this._myDropdownInput2Control.setOptions(this.years);
     this._myAutocompleteInput1Control.setOptions(this.years);
+    this._myAutocompleteInput2Control.setOptions(this.years);
   }
 
 
@@ -47,8 +49,16 @@ export class BeispieleAutocompletesAndDropdownsGenComponent extends BeispieleAut
     this.showSnackbar(`Model Value Dropdown 1: ${this._myDropdownInput1Control.getModelValue()}`);
   }
 
-
   sendAutocomplete1(): void {
     this.showSnackbar(`Model Value Autocomplete 1: ${this._myAutocompleteInput1Control.getModelValue()}`);
   }
+
+  sendDropdown2(): void {
+    this.showSnackbar(`Model Value Dropdown 2: ${this._myDropdownInput2Control.getModelValue()}`);
+  }
+
+  sendAutocomplete2(): void {
+    this.showSnackbar(`Model Value Autocomplete 2: ${this._myAutocompleteInput2Control.getModelValue()}`);
+  }
+
 }
