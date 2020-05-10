9d14aaa4daaa4c479a6fb5272cc901cecfea7645
fabalexsie <50114802+fabalexsie@users.noreply.github.com>
2020-05-10 18:01:09 +0200

    some update to beispiele-gaugechart-gen.ts
    is it used?

--- 03.FrontendRTE/src/app/beispiele/beispiele-gaugecharts-gen.component.ts
+++ 03.FrontendRTE/src/app/beispiele/beispiele-gaugecharts-gen.component.ts
@@ -16,6 +16,28 @@ export class BeispieleGaugechartsGenComponent extends BeispieleGaugechartsGenCom
     protected _route: ActivatedRoute,
     protected _commandRestService: CommandRestService) {
     super(_commandRestService, _route, _router, _webSocketService);
+    //Object.assign(this, { single });
+  }
+
+  single: any[];
+  view: any[] = [500, 400];
+  legend: boolean = true;
+  legendPosition: string = 'below';
+
+  colorScheme = {
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+  };
+
+  onSelect(data): void {
+    console.log('Item clicked', JSON.parse(JSON.stringify(data)));
+  }
+
+  onActivate(data): void {
+    console.log('Activate', JSON.parse(JSON.stringify(data)));
+  }
+
+  onDeactivate(data): void {
+    console.log('Deactivate', JSON.parse(JSON.stringify(data)));
   }
 
 }
