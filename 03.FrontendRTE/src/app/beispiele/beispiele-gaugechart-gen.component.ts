880f84aa4d185559941836db9ee4fa4491905929
fabalexsie <50114802+fabalexsie@users.noreply.github.com>
2020-05-21 21:38:44 +0200

    implement socket (Tutorial)

--- 03.FrontendRTE/src/app/beispiele/beispiele-gaugechart-gen.component.ts
+++ 03.FrontendRTE/src/app/beispiele/beispiele-gaugechart-gen.component.ts
@@ -2,6 +2,9 @@ import {Component} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
 import {CommandRestService} from '@shared/architecture/command/rte/command.rest.service';
 import {BeispieleGaugechartGenComponentTOP} from '@targetgui/beispiele-gaugechart-gen.component/beispiele-gaugechart-gen.component-top';
+import {WebSocketService} from '@shared/architecture/services/websocket.service';
+import {LineGraphDTO} from "@targetdtos/linegraph.dto";
+import {TypedJSON} from "@upe/typedjson";
 
 @Component({
     templateUrl: '../../../target/generated-sources/gui/beispiele-gaugechart-gen.component/beispiele-gaugechart-gen.component.html',
@@ -10,8 +13,23 @@ export class BeispieleGaugechartGenComponent extends BeispieleGaugechartGenCompo
     public constructor(
         protected _commandRestService: CommandRestService,
         protected _route: ActivatedRoute,
-        protected _router: Router
+        protected _router: Router,
+        protected _webSocketService: WebSocketService
     ) {
-        super(_commandRestService, _route, _router);
+        super(_commandRestService, _route, _router, _webSocketService);
+    }
+
+    public subscribedataSocket(): void {
+        if (this.dataSocket) {
+            this.subscriptions.push(this.dataSocket.subscribe(message => {
+                    let receivedData: LineGraphDTO = TypedJSON.parse(message.data, LineGraphDTO);
+                    this.data = this.transformSocketData(receivedData);
+                    console.log(message)
+                }, err =>
+                    console.error(err)
+            ));
+        } else {
+            console.error('Socket is not initialized. Initialize socket in the component constructor');
+        }
     }
 }
