beeaafd413238a99b4b1f84a9cd68afa86b920a0
Lia Schulze Dephoff <Lia.Schulze.Dephoff@rwth-aachen.de>
2020-05-21 12:10:30 +0200

    observable for data

--- 03.FrontendRTE/src/app/beispiele/beispiele-densitycharts-gen.component.ts
+++ 03.FrontendRTE/src/app/beispiele/beispiele-densitycharts-gen.component.ts
@@ -3,20 +3,26 @@ import {Component, OnInit, QueryList, ViewChildren} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import { DensityChartComponent } from "@components/charts/density-chart/density-chart.component";
 import {CommandRestService} from '@shared/architecture/command/rte/command.rest.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Data2Model } from '../data/data.model';
 import {BeispieleDensitychartGenComponent} from '@targetgui/beispiele-densitychart-gen.component/beispiele-densitychart-gen.component-top';
  import {WebSocketService} from "@services/websocket.service";
 @Component({
   templateUrl: '../../../target/generated-sources/gui/beispiele-densitychart-gen.component/beispiele-densitychart-gen.component.html',
 })
 export class BeispieleDensitychartsGenComponent extends BeispieleDensitychartGenComponent {
+  data2: Observable<Data2Model>;
 
-    public constructor(
+  constructor(private http: HttpClient,
         protected _webSocketService: WebSocketService,
         protected _router: Router,
         protected _route: ActivatedRoute,
         protected _commandRestService: CommandRestService) {
-        super(_commandRestService, _route, _router/*, _webSocketService*/);
-    }
+        
+	super(_commandRestService, _route, _router/*, _webSocketService*/)
+   	this.data2 = this.http.get<Data2Model>('./assets/data2.json');
+  }
     title = 'Densitychart';
 
   public data =
