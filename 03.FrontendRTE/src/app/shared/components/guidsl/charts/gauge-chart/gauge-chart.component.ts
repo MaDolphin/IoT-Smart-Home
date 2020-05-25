d67d16a97df4dea40ec906636433459ff9d556e0
fabalexsie <50114802+fabalexsie@users.noreply.github.com>
2020-05-25 11:52:57 +0200

    Fix: Anti-Resize-Container + option to change scale
    some comments

--- 03.FrontendRTE/src/app/shared/components/guidsl/charts/gauge-chart/gauge-chart.component.ts
+++ 03.FrontendRTE/src/app/shared/components/guidsl/charts/gauge-chart/gauge-chart.component.ts
@@ -10,33 +10,22 @@ import {GaugeDummyData} from "@components/charts/gauge-chart/gauge-dummy-data";
 export class GaugeChartComponent implements OnInit {
     @Input() textValue: string;
     @Input() bigTicks: number = 10;
-	@Input() smallTicks: number = 5;
-	@Input() unit: string;
+    @Input() smallTicks: number = 5;
+    @Input() unit: string;
     @Input() min: number = 0;
     @Input() max: number = 100;
 
-    view: any[] = [400, 400]; // todo change dynamically with screen size
-	showAxis: boolean = true;
+    view: any[] = [400, 400];
     legend: boolean = true;
     showText: boolean = true;
 
-    public options: any = {};
-    public dataSet: any[] = [{
-            'name': 'Kitchen',
-            'value': 23
-        },
-        {
-            'name': 'Bathroom',
-            'value': 30
-        },
-        {
-            'name': 'Bedroom',
-            'value': 21
-        },
-        {
-            'name': 'Office',
-            'value': 15
-        }];
+    // realData(Min|Max) is used to set the end of the slider
+    realDataMin: number;
+    realDataMax: number;
+    selectedMin: number;
+    selectedMax: number;
+
+    public dataSet: any[] = [];
     legendPosition: string = 'below';
 
     @Input()
@@ -46,20 +35,31 @@ export class GaugeChartComponent implements OnInit {
 
     ngOnInit(): void {
         this.showText = !!this.textValue;
+        // to force resetting when first data is load
+        this.realDataMin = this.max;
+        this.realDataMax = this.min;
+
+        // select biggest span
+        this.selectedMin = this.min;
+        this.selectedMax = this.max;
+
+        this.showAxis = true;
     }
 
     constructor() {
     }
 
     counter: number = 0;
+
     @Input()
     public set data(lineData: LineDataGroup[]) {
-        // todo only replace changed data
+        // todo only replace changed data, don't replace all
         let t = this;
 
-        let useDummyData = false;
+        // use the dummyData from the LineGraph or generate new one for the gauge chart
+        let useGivenData = false;
 
-        if (useDummyData) {
+        if (useGivenData) {
             lineData.forEach(function (lineD: LineDataGroup, i: number) {
                 if (lineD.label != null && lineD.data.length > 0) {
                     console.log(lineD.label + ": " + lineD.data[0].y);
@@ -69,12 +69,32 @@ export class GaugeChartComponent implements OnInit {
             });
         } else {
             this.counter++;
-            this.counter %= 2; // reduce refresh rate
+            this.counter %= 2; // reduces refresh rate
             if (this.counter == 0) {
-                //console.log(GaugeDummyData.getNewData());
+                //console.log(GaugeDummyData.getNewData(this.min, this.max));
                 t.dataSet = GaugeDummyData.getNewData(this.min, this.max);
             }
         }
+
+        // Reset realDataMin and realDataMax
+        t.dataSet.forEach(function (entry: any, i: number) {
+            if (entry.value < t.realDataMin) t.realDataMin = Math.round(entry.value);
+            if (entry.value > t.realDataMax) t.realDataMax = Math.round(entry.value);
+        })
+    }
+
+
+    private _showAxis: boolean;
+    public set showAxis(checked: boolean){
+        if(!checked){
+            this.selectedMin = this.min; // select biggest span
+            this.selectedMax = this.max; // select biggest span
+        }
+        this._showAxis = checked;
+    }
+
+    public get showAxis() : boolean{
+        return this._showAxis;
     }
 
     public hasData(): boolean {
