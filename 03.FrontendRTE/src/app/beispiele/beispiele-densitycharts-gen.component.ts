d00c5d3c43eccf0aa3faa215520234904a333850
Lia Schulze Dephoff <Lia.Schulze.Dephoff@rwth-aachen.de>
2020-06-04 15:12:14 +0200

    fixed data mocking to the correct receiver

--- 03.FrontendRTE/src/app/beispiele/beispiele-densitycharts-gen.component.ts
+++ 03.FrontendRTE/src/app/beispiele/beispiele-densitycharts-gen.component.ts
@@ -5,14 +5,12 @@ import { DensityChartComponent } from "@components/charts/density-chart/density-
 import {CommandRestService} from '@shared/architecture/command/rte/command.rest.service';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Data2Model } from '../data/data.model';
 import {BeispieleDensitychartGenComponent} from '@targetgui/beispiele-densitychart-gen.component/beispiele-densitychart-gen.component';
 import {WebSocketService} from "@services/websocket.service";
 @Component({
     templateUrl: '../../../target/generated-sources/gui/beispiele-densitychart-gen.component/beispiele-densitychart-gen.component.html',
 })
 export class BeispieleDensitychartsGenComponent extends BeispieleDensitychartGenComponent {
-    public data2: Data2Model [];
     constructor(private http: HttpClient,
                 protected _webSocketService: WebSocketService,
                 protected _router: Router,
@@ -26,7 +24,7 @@ export class BeispieleDensitychartsGenComponent extends BeispieleDensitychartGen
     public subscribedataSocket(): void {
         if(this.dataSocket) {
             this.subscriptions.push(this.dataSocket.subscribe(message => {
-                this.data2 = this.mockData();
+                this.densitysensordata = this.mockData();
             }, err => console.error(err)));
         } else {
             console.error('Socket is not initialied. initialize socket in component constructor');
