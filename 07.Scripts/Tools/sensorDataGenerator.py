73087881eae6bd8754b90a9b407c448b5e455d59
an.lukastim <lukas.tim.zimmermann@rwth-aachen.de>
2020-06-08 12:30:07 +0200

    Fixes backend task #5, spelling error

--- 07.Scripts/Tools/sensorDataGenerator.py
+++ 07.Scripts/Tools/sensorDataGenerator.py
@@ -1,9 +1,10 @@
 import json
+import time
 import datetime
 import requests
 
 payload = []
-timeStamp = 1577862000
+timeStamp = int(time.time())
 payloadString = '{"data":['
 
 
@@ -13,66 +14,96 @@ for i in range(0,1000):
 		sensorId = '1'
 		type = 'CO2'
 		value = 0.035
+		timeStamp += 1
 	elif(i <= 100):
 		sensorId = '1'
 		type = 'CO2'
 		value = value + 0.1
+		timeStamp += 1
 	elif(i <= 190):
 		sensorId = '1'
 		type = 'CO2'
 		value = value+1
+		timeStamp += 1
 	elif(i <= 200):
 		sensorId = '1'
 		type = 'CO2'
-		value = 90;	
+		value = 90;
+		timeStamp += 1
 	elif(i == 201):
 		sensorId = '2'
 		type = 'TEMPERATURE'
 		value = 10
+		timeStamp = int(time.time())
 	elif(i <= 300):
 		sensorId = '2'
 		type = 'TEMPERATURE'
 		value = value + 0.1
+		timeStamp += 1
 	elif(i <= 400):
 		sensorId = '2'
 		type = 'TEMPERATURE'
 		value = value+0.5
+		timeStamp += 1
 	elif(i <= 401):
 		sensorId = '3'
 		type = 'LIGHT'
 		value = 5
+		timeStamp = int(time.time())
 	elif(i <= 496):
 		sensorId = '3'
 		type = 'LIGHT'
 		value = value + 1
+		timeStamp += 1
 	elif(i <= 550):
 		sensorId = '3'
 		type = 'LIGHT'
 		value = 100
+		timeStamp += 1
 	elif(i <= 640):
 		sensorId = '3'
 		type = 'LIGHT'
 		value = value - 1
+		timeStamp += 1
 	elif(i <= 700):
 		sensorId = '3'
 		type = 'LIGHT'
 		value = 5
+		timeStamp+=1
+	elif(i == 701):
+	    sensorId = '4'
+	    type = 'PERCENT'
+	    value = 0
+	    timeStamp = int(time.time())
 	elif(i <= 800):
-		sensorId = '4'
-		type = 'PERCENT'
-		value = 0
+	    sensorId = '4'
+	    type = 'PERCENT'
+	    value = 0
+	    timeStamp += 1
+	elif(i == 801):
+	    sensorId = '4'
+	    type = 'MOTION'
+	    value = 0
+	    timeStamp = int(time.time())
 	elif(i <= 900):
 		sensorId = '5'
 		type = 'MOTION'
 		value = 0
+		timeStamp+=1
+	elif(i == 901):
+	    sensorId = '4'
+	    type = 'ANGLE'
+	    value = 0
+	    timeStamp = int(time.time())
 	else:
 		sensorId = '6'
 		type = 'ANGLE'
 		value = 90
+		timeStamp+=1
 	print('Sensor data #' + str(i), end='\r')	
 	payloadString += f'{{"sensorId":"{sensorId}","type":"{type}","value":{value},"timeStamp":"{str(datetime.datetime.utcfromtimestamp(timeStamp))}"}}' 
 	payloadString += ','
-	timeStamp += 1
+
 
 print('Payload created succesfully')
 
